a40014d0df2f9b1ddb23032443e6302011f1c007
KyronStorm18 <66709580+KyronStorm18@users.noreply.github.com>
2022-12-01 18:15:58 -0500

    deployed my react app to github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,30 @@ function App() {
     <div className="App">
       <h1>Lab 10 Problem 3: Web Dev Framework </h1>
       <h2>By: Kyron</h2>
+      <p>As you can see from the title in the tabs, I'm using React as my framework.</p>
+      <section>
+      <h3>How I installed and Setup my framework</h3>  
+      <ol>
+      <li>Download Node.js and Git online.</li>
+      <li>Create a folder where I want to store my React website.</li>
+      <li>Create a React application using the command "npm init react-app react-deploy" in terminal. </li>
+      <li>Make a repository in Github. This repository is where I will store my code for my website.</li>
+      <li>Connect my React website with the provided Git commands found in the newly created repository.</li>
+      <li>Commit I React website once I finished editing the code. </li>
+      <li>Push my React website into the git repository using the command "git push -u origin master". </li>
+      <li>Find my URL in the Settings of the repository ("https://KyronStorm18.gihtub.io/react-deploy/").</li>
+      </ol>
+      </section>
+
+      <section>
+        <h3>How I Built the Webpage Using the Framework</h3>
+        <p>There wasn't too much that had to built since the react application that was used was already put together. The only things that had to be "built" is the contents within the js file and css file where you can add whatever you want to personalize the website.</p>
+      </section>
+
+      <section>
+        <h3>Challenges I Encountered</h3>
+        <p>I didn't know at first how to commit code to a repository at first. Additionally, being my first time dealing with React I was a little concerned on how I would put everything together. However, with the power of Google and Youtube, I was able to find quick and easy tutorials which helped me setup all I needed.</p>
+      </section>
     </div>
   );
 }
