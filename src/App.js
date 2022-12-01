b014d4c9893e323bc571be21646d1aa10e31b1a9
KyronStorm18 <66709580+KyronStorm18@users.noreply.github.com>
2022-12-01 16:55:58 -0500

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lab 10 Problem 3: Web Dev Framework </h1>
+      <h2>By: Kyron</h2>
     </div>
   );
 }
